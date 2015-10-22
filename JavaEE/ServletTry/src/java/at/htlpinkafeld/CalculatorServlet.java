ab1e586a41329d3a429b297fc99c73c72c97a823
Dogist <ma.six@gmx.at>
2015-10-22 10:13:15 +0200

    First GitProject(07.10.15): Calculator verbessert

--- JavaEE/ServletTry/src/java/at/htlpinkafeld/CalculatorServlet.java
+++ JavaEE/ServletTry/src/java/at/htlpinkafeld/CalculatorServlet.java
@@ -29,10 +29,56 @@ public class CalculatorServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
+        String res = "";
+        String pluSel = "", minSel = "", mulSel = "", divSel = "";
+        String leftO="", rightO="";
+        String opS = request.getParameter("operator");
+        if(opS!=null)
+        try {
+            leftO = request.getParameter("leftO");
+            Double l;
+            if (leftO != null) {
+                l = Double.parseDouble(leftO);
+            } else {
+                l = 1.0;
+            }
+            rightO = request.getParameter("rightO");
+            Double r;
+            if (leftO != null) {
+                r = Double.parseDouble(rightO) ;
+            } else {
+                r = 1.0;
+            }
+            
+            switch (opS) {
+                case "+":
+                    res = "" + (l + r);
+                    pluSel = "selected";
+                    break;
+                case "-":
+                    res = "" + (l - r);
+                    minSel = "selected";
+                    break;
+                case "*":
+                    res = "" + l * r;
+                    mulSel = "selected";
+                    break;
+                case "/":
+                    res = "" + l / r;
+                    divSel = "selected";
+                    break;
+                default:
+                    res = "unbekannter Fehler";
+            }
+        } catch (NumberFormatException e) {
+            res = "Invalid Number inputted";
+        } catch (Exception e) {
+            res = "Error";
+            System.out.println(e);
+        }
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
@@ -42,7 +88,7 @@ public class CalculatorServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Calculator</h1>");
-            out.println("<form method=\"get\" action=\"CalculatorServlet\">\n");
+            out.println("<form method=\"get\" >\n");
             out.println("<table border=\"0\" width=\"100% \">\n");
             out.println("<colgroup>\n");
             out.println("<col width=\"150\">\n");
@@ -50,45 +96,26 @@ public class CalculatorServlet extends HttpServlet {
             out.println("</colgroup>\n");
             out.println("        	<tr>\n");
             out.println("          	<td align=\"left\">Left operand:</td>\n");
-            out.println("          	<td align=\"left\"><input type=\"number\" name=\"leftO\" value=\"" + request.getParameter("leftO") + "\"></td>\n");
+            out.println("          	<td align=\"left\"><input  name=\"leftO\" value=\"" + leftO + "\"></td>\n");
             out.println("        	</tr>\n");
             out.println("        	<tr>\n");
             out.println("          	<td align=\"left\">Right operand:</td>\n");
-            out.println("          	<td align=\"left\"><input type=\"number\" name=\"rightO\" value=\"" + request.getParameter("rightO") + "\"></td>\n");
+            out.println("          	<td align=\"left\"><input  name=\"rightO\" value=\"" + rightO + "\"></td>\n");
             out.println("        	</tr>\n");
             out.println("        	<tr>\n");
             out.println("				<td align=\"left\"><input type=\"submit\" value=\"Calculate\"></td>\n");
             out.println("          		<td align=\"left\">\n");
             out.println("					<select name=\"operator\">\n");
-            out.println("						<option value=\"+\">+</option>\n");
-            out.println("				  		<option value=\"-\">-</option>\n");
-            out.println("						<option value=\"*\">*</option>\n");
-            out.println("						<option value=\"/\">/</option>\n");
+            out.println("						<option value=\"+\" " + pluSel + ">+</option>\n");
+            out.println("				  		<option value=\"-\" " + minSel + ">-</option>\n");
+            out.println("						<option value=\"*\" " + mulSel + ">*</option>\n");
+            out.println("						<option value=\"/\" " + divSel + ">/</option>\n");
             out.println("					</select>		  \n");
             out.println("               </td>\n");
             out.println("           </tr>\n");
             out.println("           <tr>\n");
             out.println("                       <td align=\"left\">Result:</td>\n");
-            if (request.getParameter("leftO") != null && request.getParameter("rightO") != null) {
-                int leftO = Integer.parseInt(request.getParameter("leftO"));
-                int rightO = Integer.parseInt(request.getParameter("rightO"));
-                int res = 0;
-                switch (request.getParameter("operator")) {
-                    case "+":
-                        res = leftO + rightO;
-                        break;
-                    case "-":
-                        res = leftO - rightO;
-                        break;
-                    case "*":
-                        res = leftO * rightO;
-                        break;
-                    case "/":
-                        res = leftO / rightO;
-                        break;
-                }
-                out.println("<td align=\"left\">" + res + "</td>");
-            }
+            out.println("<td align=\"left\">" + res + "</td>");
             out.println("           </tr>\n");
             out.println(" </table>\n");
             out.println(" </form>");
@@ -97,7 +124,7 @@ public class CalculatorServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
